5758d20f6e47545f10813224cd534c5559dbc131
Aashish Shilkande <arshilkande999@gmail.com>
2026-02-14 23:54:07 +0530

    feat: implement user authentication with jwt

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // health check route
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { registerUser, loginUser } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+// Register User
+router.post('/register', registerUser);
+// Login User
+router.post('/login', loginUser);
+
+export default router;
